5da83725854238178e630ed16e8b174e0b59ec5f
hermes7144 <kichancho@gmail.com>
2022-11-19 16:35:14 +0900

    update, delete, filter 기능 추가, css 꾸미기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import Todo from './components/Todo';
+import { useState } from 'react'
+import Header from './components/Header/Header';
+import TodoList from './components/TodoList/TodoList';
+
+const filters = ['all', 'active', 'completed']
 function App() {
+  const [filter, setFilter] = useState(filters[0]);
+
+
   return (
-    <div>
-      <Todo />
-    </div>
+    <>
+      <Header
+        filters={filters}
+        filter={filter}
+        onFilterChange={setFilter}
+      />
+      <TodoList filter={filter} />
+    </>
   );
 }
 
